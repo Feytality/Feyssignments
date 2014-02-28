8d1851d65179339894d2958822f60db2b71f722e
niofire <stlouis_mathieu@hotmail.com>
2014-02-28 17:23:12 -0500

    Update PriorityQueue.java

--- 331_a2/src/PriorityQueue.java
+++ 331_a2/src/PriorityQueue.java
@@ -1,4 +1,34 @@
-
 public class PriorityQueue {
-
-}
+  
+  public PriorityQueue()
+  {
+    
+  }
+  
+  public int size()
+  {
+    
+  }
+  
+  public boolean isEmpty()
+  {
+    
+  }
+  
+  public void insert(int Key, String Element)
+  {
+    
+  }
+  
+  public Node getMax()
+  {
+    
+  }
+  
+  public Node remove()
+  {
+    
+  }
+  
+  
+  }
